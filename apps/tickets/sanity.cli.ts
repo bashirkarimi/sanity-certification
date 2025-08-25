1c3fd347d9ba70e9aa07e2944390743d33e65fc0
Muhammad Bashir <bashirkarimi5@gmail.com>
2025-08-25 09:01:22 +0200

    feat: add id field

--- apps/tickets/sanity.cli.ts
+++ apps/tickets/sanity.cli.ts
@@ -2,7 +2,8 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   app: {
-    organizationId: 'ouk3ETE6T',
-    entry: './src/App.tsx',
+    organizationId: "ouk3ETE6T",
+    entry: "./src/App.tsx",
+    id: "zofflb2la9tnzmz454dsosel",
   },
-})
+});
