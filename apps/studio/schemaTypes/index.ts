88d2020aef75ce8652429f748f8b9383b6e20d88
Muhammad Bashir <bashirkarimi5@gmail.com>
2025-08-29 14:58:41 +0200

    feat: add home page and site settings

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,9 +1,19 @@
-import { eventType } from "./eventType";
-import { artistType } from "./artistType";
-import { venueType } from "./venueType";
-import { feedbackType } from "./feedbackType";
-import { heroType } from "./heroType";
-import { pageBuilderType } from "./pageBuilderType";
-import { pageType } from "./pageType";
+import {eventType} from './eventType'
+import {artistType} from './artistType'
+import {venueType} from './venueType'
+import {feedbackType} from './feedbackType'
+import {heroType} from './heroType'
+import {pageBuilderType} from './pageBuilderType'
+import {pageType} from './pageType'
+import {siteSettingsType} from './siteSettingsType'
 
-export const schemaTypes = [eventType, artistType, venueType, feedbackType, heroType, pageBuilderType, pageType]
+export const schemaTypes = [
+  eventType,
+  artistType,
+  venueType,
+  feedbackType,
+  heroType,
+  pageBuilderType,
+  pageType,
+  siteSettingsType,
+]
--- apps/web/src/sanity/lib/queries.ts
+++ apps/web/src/sanity/lib/queries.ts
@@ -1,8 +1,18 @@
 import { defineQuery } from "next-sanity";
 
-export const PAGE_QUERY = defineQuery(`*[_type == "page" && slug.current == $slug][0] {
+export const PAGE_QUERY =
+  defineQuery(`*[_type == "page" && slug.current == $slug][0] {
   _id,
   title,
   slug,
   content
 }`);
+
+export const HOME_PAGE_QUERY = defineQuery(`*[_id == "siteSettings"][0]{
+    homePage->{
+      ...,
+      content[]{
+        ...,
+      }
+    }
+  }`);
